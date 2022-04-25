958816c8be3515ec87303d1043677b63835680e3
jacobconklin <jacobconklin@vt.edu>
2022-04-25 15:17:04 -0400

    Added the types just for refrence but we can delete them before submitting

---
+++ APISimulator/models/result.ts
@@ -0,0 +1,6 @@
+
+export class Result {
+  HomeScore: number;
+  AwayScore: number;
+  Final: boolean; // true if home won, false if away won
+}
--- APISimulator/services/game.service.js
+++ APISimulator/services/game.service.js
@@ -1,8 +1,35 @@
+import {Game} from '../models/game';
 
 module.exports = {
     getGames,
     getResult
 }
+
+// Here will be all of the Games and their results:
+
+const games = [{
+    id: '1',
+    sport: Sport.basketball,
+    time: new Date(Date.now()), // Maybe will come as a string we can copy or a Date
+    homeTeam: 'Toronto Raptors', // Could make 'Team's their own Model but I think it's unnecessary
+    homeSpread: -3.0, // These are the values we would get from jsonodds.com
+    homeSpreadLine: -110,
+    homeMoneyLine: -153,
+    awayTeam: 'Chicago Bulls',
+    awaySpread: 3.0,
+    awaySpreadLine: 110,
+    awayMoneyLine: 132,
+    totalNumber: 182.5,
+    over: -110,
+    under: -110,
+    result: {
+        HomeScore: 100,
+        AwayScore: 110,
+        Final: false,
+    }
+}];
+
+
 
 async function getGames(sport) {
 
